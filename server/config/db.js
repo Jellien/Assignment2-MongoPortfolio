6548be1474ab9a2674063c9ac4577aa5d0ca8c28
Julian Vick <JM@romtec.info>
2022-10-24 22:26:10 -0400

    updated URI to work with mongo atlast

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = 
 {
-    "URI": "mongodb://127.0.0.1/contact_list"
+    "URI": "mongodb+srv://julian:2DnILlZfaMMQI5Pr@cluster0.uluztu8.mongodb.net/contact_list?retryWrites=true&w=majority"
 }
